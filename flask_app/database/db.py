410568a29cd5f7fcfe42dad287e51331b4ef85e3
Biduranr <benjamin.duran@ug.uchile.cl>
2026-05-07 19:41:35 -0400

    Model: Definicion de modelos SQLAlchemy en db.py y carga de datos SQL

---
+++ flask_app/database/db.py
@@ -0,0 +1,42 @@
+from flask_sqlalchemy import SQLAlchemy 
+
+db = SQLAlchemy()
+
+class Region(db.Model):
+    __tablename__ = 'region'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nombre = db.Column(db.String(200), nullable=False)
+
+class Comuna(db.Model):
+    __tablename__ = 'comuna'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nombre = db.Column(db.String(200), nullable=False)
+    region_id = db.Column(db.Integer, db.ForeignKey('region.id'), nullable=False)
+
+class Miembro(db.Model):
+    __tablename__ = 'miembro'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nombre = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(80), nullable=False)
+    telefono = db.Column(db.String(15), nullable=False)
+    fecha_registro = db.Column(db.DateTime, nullable=False)
+    comuna_id = db.Column(db.Integer, db.ForeignKey('comuna.id'), nullable=False)
+
+class Actividad(db.Model):
+    __tablename__ = 'actividad'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    miembro_id = db.Column(db.Integer, db.ForeignKey('miembro.id'), nullable=False)
+    dia = db.Column(db.enum('Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'), nullable=False)
+    hora_inicio = db.Column(db.String(5), nullable=False)
+    duracion = db.Column(db.String(5), nullable=False)
+    tipo_actividad = db.Column(db.enum('arte', 'deporte', 'tecnologia', 'social', 'recreacion', 'otra'), nullable=False)
+    nombre = db.Column(db.String(45), nullable=False)
+    descripcion = db.Column(db.Text)
+
+class Foto(db.Model):
+    __tablename__ = 'foto'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    ruta_archivo = db.Column(db.String(300), nullable=False)
+    nombre_archivo = db.Column(db.String(300), nullable=False)
+    actividad_id = db.Column(db.Integer, db.ForeignKey('actividad.id'), nullable=False)
+    
